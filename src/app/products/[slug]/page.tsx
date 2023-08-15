66f7fc73af43b5f0b67646d9fce557f9943e5c71
jkpark104 <jkrang104@gmail.com>
2023-08-15 21:57:23 +0900

    feat: generate-static-params

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -7,3 +7,9 @@ type Props = {
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} Page</h1>;
 }
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+
+  return products.map((slug) => ({ slug }));
+}
