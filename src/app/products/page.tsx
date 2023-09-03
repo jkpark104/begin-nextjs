fff600b24e78d81458045195c931ad27b0da5228
jkpark104 <jkrang104@gmail.com>
2023-09-03 22:14:06 +0900

    feat: fetch isr, ssg

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,21 @@
 import { getProducts } from "@/services/products";
 import Link from "next/link";
+import styles from "./page.module.css";
+
+// export const revalidate = 3;
 
 export default async function ProductsPage() {
   const products = await getProducts();
   // 서버 파일(DB)에 있는 제품의 리스트를 일거와서 그것을 보여주기
+
+  const res = await fetch("https://meowfacts.herokuapp.com/", {
+    next: { revalidate: 3 }, // 0일 때는 SSR로 동작
+    // cache: "no-store", // 캐시를 사용하지 않겠다. -> SSR,
+  });
+
+  const data = await res.json();
+
+  const factText = data.data[0];
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -16,6 +28,7 @@ export default async function ProductsPage() {
           );
         })}
       </ul>
+      <article className={styles.article}>{factText}</article>
     </>
   );
 }
